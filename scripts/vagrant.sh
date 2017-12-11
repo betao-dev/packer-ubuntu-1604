6e29d554849d52b10b1ea7201c04a9b16d1fbfc8
Fedor Paligin <f.paligin@gmail.com>
2017-12-11 11:48:18 +0700

    Last adjustments

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -4,4 +4,5 @@ mkdir /home/vagrant/.ssh
 wget --no-check-certificate https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chmod 700 /home/vagrant/.ssh
 chmod 600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+chown -R vagrant:vagrant /home/vagrant
+chown -R vagrant:vagrant /opt/virtualenv
